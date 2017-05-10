625fed8a874d2a7cf352c7075f7336b00a5ce743
Ark <digitalshadow2.0@gmail.com>
2017-05-10 14:56:41 -0300

    Something finished

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -1,13 +1,34 @@
 package com.acme.csci3130;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static com.acme.csci3130.R.layout.activity_main;
 
 public class MainActivity extends Activity {
-
+    public static final String Extra_Message = "com.acme.csci3130.MESSAGE";
+    EditText editText;
+    TextView toShowIt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+        editText = (EditText) findViewById(R.id.editText);
+        toShowIt = (TextView) findViewById(R.id.DisplayText);
+    }
+
+    public void changeWords (View view){
+        //editText.setText("Found");
+        String message = editText.getText().toString();
+        toShowIt.setText(message);
+
     }
 }
